1653927f86c1a8314ade8902b6df3296a68f14bb
aditgiant <adityagianto@gmail.com>
2021-03-14 18:59:13 +0700

    update SEO

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -7,8 +7,8 @@ import '@assets/css/icomoon.css';
 import '@assets/css/bootstrap.css';
 import '@assets/css/style.css';
 
-const IMAGE_URL = `https://thekusuma.com/static/slide-6-4715e29302dbaa2ba21494c6258298d4.jpg`;
-const META_DESCRIPTION = `Dengan memohon Rahmat dan Ridho Illahi, teriring niat menjalankan Sunnah Rasulullah ﷺ untuk membentuk rumah tangga yang Sakinah, Mawaddah wa Rahmah, kami mohon do'a agar senantiasa diberikan kelancaran dan keberkahan. - Bella & Hendy`;
+// const IMAGE_URL = `https://thekusuma.com/static/slide-6-4715e29302dbaa2ba21494c6258298d4.jpg`;
+const META_DESCRIPTION = `Our love stories :)`;
 
 function MainLayout({ children }) {
   return (
@@ -21,16 +21,15 @@ function MainLayout({ children }) {
 
         {/* font and SEO tags */}
         <meta property="og:title" content="The Wedding of Bella & Hendy" />
-        <meta property="og:image" content={IMAGE_URL} />
-        <meta property="og:url" content="https://thekusuma.com" />
+        <meta property="og:image" content="./bella-hendy.jpg"/>
+        <meta property="og:url" content="https://bellahendy.com" />
         <meta property="og:site_name" content="The Wedding of Bella & Hendy" />
         <meta property="og:description" content={META_DESCRIPTION} />
         <meta name="twitter:title" content="The Wedding of Bella & Hendy" />
         <meta name="twitter:description" content={META_DESCRIPTION} />
         <meta name="twitter:image" content={IMAGE_URL} />
-        <meta name="twitter:url" content="https://thekusuma.com" />
+        <meta name="twitter:url" content="https://bellahendy.com" />
         <meta name="twitter:card" content="summary" />
-        <meta name="twitter:creator" content="@idindrakusuma" />
 
         <link
           href="https://fonts.googleapis.com/css?family=Work+Sans:400,300,600,400italic,700"
