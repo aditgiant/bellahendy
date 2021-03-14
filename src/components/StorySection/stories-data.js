1cae0d668f99906af8afc41aef6378bc2fda7479
aditgiant <adityagianto@gmail.com>
2021-03-14 19:35:39 +0700

    update story

--- src/components/StorySection/stories-data.js
+++ src/components/StorySection/stories-data.js
@@ -9,35 +9,35 @@ export const stories = [
     title: ' OUR FIRST MEET UP',
     date: '2017',
     description:
-      'Pertama kali bertemu September 2017 di kantor. Awalnya B&H hanya kenal sebagai kenal teman kerja 🤣',
+      'Pertama kali bertemu September 2017 di kantor. Awalnya Bella & Hendy hanya kenal sebagai kenal teman kerja di Telkomsel Tasikmalaya 🤣',
     image: ImgNdodokLawang,
   },
   {
     title: 'WE ARE DATING',
     date: '2018',
     description:
-      'Seiring waktu berjalan, B&H semakin dekat karena pergi bersama, cari makan bareng dan 1 tempat kos (makanya bareng mulu hahahah). Dari situ, B&H cerita perasaan masing-masing tanggal 1 September 2018 dan memutuskan mulai pacaran.',
+      'Seiring waktu berjalan, Bella & Hendy semakin dekat karena pergi bersama, cari makan bareng dan 1 tempat kos (makanya bareng mulu hahahah). Dari situ, mereka cerita perasaan masing-masing tanggal 1 September 2018 dan memutuskan mulai pacaran.',
     image: ImgWearedating,
   },
   {
     title: 'LDR',
     date: '2019 - 2021',
     description:
-      'Baru berapa bulan pacaran, B&H harus siap untuk menghadapi LDR. Pada tanggal 1 Maret 2019 B&H mulai ber-LDR karena B melanjutkan pendidikan S2 di Korea. Walau mulanya B&H tak yakin untuk menghadapi ini selama 2 tahun, tetapi alhamdulliah, mungkin namanya jodoh B&H bisa menjaga perasaan masing-masing dan menetapkan untuk melangkah ke tahap lebih serius.',
+      'Baru berapa bulan pacaran, Bella & Hendy harus siap untuk menghadapi LDR. Pada tanggal 1 Maret 2019 mereka mulai ber-LDR karena Bella melanjutkan pendidikan S2 di Korea. Walau mulanya mereka tak yakin untuk menghadapi ini selama 2 tahun, tetapi alhamdulliah, mungkin namanya jodoh mereka bisa menjaga perasaan masing-masing dan menetapkan untuk melangkah ke tahap lebih serius.',
     image: ImgJakartaKroasia,
   },
   {
     title: 'CANCEL ENGAGEMENT DAY',
     date: '2020',
     description:
-      'Karena kedua keluarga sudah saling bertemu dan merestui hubungan, B&H berencana untuk lamaran pada Agustus 2020. Tetapi Allah punya rencana lain, lamaran diundur karena COVID-19 ke Februari 2021. Tapi, karena keterbatasan waktu lamaran dibatalkan.',
+      'Karena kedua keluarga sudah saling bertemu dan merestui hubungan, Bella & Hendy berencana untuk lamaran pada Agustus 2020. Tetapi Allah punya rencana lain, lamaran diundur karena COVID-19 ke Februari 2021. Tapi, karena keterbatasan waktu lamaran dibatalkan.',
     image: ImgBoarding,
   },
   {
     title: 'OUR WEDDING DAY',
     date: '2021',
     description:
-      'Menemukan pasangan yang sempurna adalah hal yang sulit, B&H sadar banyak perbedaan dalam diri masing-masing. Meskipun begitu, B&H siap menghadapinya dan mengikat janji suci sehidup-semati di hadapan Allah, keluarga, serta sahabat tercinta. B&H berjanji untuk saling menyayangi, melengkapi, menerima baik dan buruk sampai tiba di surga-Nya.',
+      'Menemukan pasangan yang sempurna adalah hal yang sulit, Bella & Hendy sadar banyak perbedaan dalam diri masing-masing. Meskipun begitu, mereka siap menghadapinya dan mengikat janji suci sehidup-semati di hadapan Allah, keluarga, serta sahabat tercinta. Mereka berjanji untuk saling menyayangi, melengkapi, menerima baik dan buruk sampai tiba di surga-Nya.',
     image: ImgKembaliKeIndonesia,
   }
 ];
